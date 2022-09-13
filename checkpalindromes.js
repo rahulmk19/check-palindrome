92d5c9de3edd51d263e678d3a4614634ff5c4ab5
deepak chourasiya <deepak1812002@gmail.com>
2022-09-13 22:45:38 +0530

    check palindrome

--- checkpalindromes.js
+++ checkpalindromes.js
@@ -1,1 +1,12 @@
+
 let string="Naman";
+
+let newstr = '';
+for(let i=string.length-1;i>=0;i--){
+    newstr+=string[i];
+}
+if(string==newstr){
+    console.log('Palindrom yes')
+}else{
+    console.log('NOt a plindrome')
+};
